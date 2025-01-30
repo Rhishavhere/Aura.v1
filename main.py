68980d3036204e870c7f98f72e1bef9510f98e9d
Rhishavhere <rhishh.me@gmail.com>
2025-01-31 00:23:48 +0530

    Working

--- main.py
+++ main.py
@@ -1,30 +1,47 @@
 from ollama import chat
-from PIL import ImageGrab
+from PIL import ImageGrab, Image
 import cv2
 import pyperclip
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 web_cam = cv2.VideoCapture(0)
-
 MODEL_NAME = 'hf.co/cognitivecomputations/dolphin-2.9.4-llama3.1-8b-gguf:Q5_K_M'
 history = []
 
-def gen(prompt):
-  system = ('Answer in short and very brief. do not exceed one line')
+genai.configure(api_key = os.getenv('GEMINI_API'))
+genai_config = {
+  'temperature':0.7,
+  'top_p':1,
+  'top_k':1,
+  'max_output_tokens':2048
+}
+model = genai.GenerativeModel('gemini-1.5-flash-latest', generation_config=genai_config)
 
-  messages = [{'role':'system','content': system}]
-  messages.extend(history)
-  messages.append({'role':'user','content': prompt})
+system_msg = (
+  'You are a multi-modal AI voice assistant. Your user may or may not have attached a photo for context '
+  '(either a screenshot or a webcam capture). Any photo has already been processed into a highly detailed '
+  'text prompt that will be attached to their transcribed voice prompt. Generate the most useful and '
+  'factual response possible, carefully considering all previous generated text in your response before '
+  'adding new tokens to the response. Do not expect or request images, just use the context if added. '
+  'Use all of the context of this conversation so your response is relevant to the conversation. Make '
+  'your responses clear and concise, avoiding any verbosity. ANSWER IN VERY SHORT AND BRIEF. DO NOT OVER EXPLAIN'
+)
+convo = [{'role':'system', 'content':system_msg}]
+
+def gen(prompt, img_context):
+  if img_context:
+    prompt = f'USER PROMPT: {prompt}\n\n  IMAGE CONTEXT: {img_context}'
+  convo.append({'role':'user','content':prompt})
   
-  try:
-    response = chat(model=MODEL_NAME, messages=messages)
+  response = chat(model=MODEL_NAME, messages=convo)
+  convo.append({'role':'assistant', 'content': response.message.content})
+  return response.message.content
 
-    history.append({'role':'user','content': prompt})
-    history.append({'role':'assistant','content': response.message.content})
-    return response.message.content
-  except Exception as e:
-    print(f'ERROR : {e}')
-
-def call(prompt):
+def function_call(prompt):
   system = (
     'You are an AI function callling model. You will determine whether extracting the users clipboard content, '
     'taking a screenshot, capturing the webcam or calling no functions is best for a voice assistant to respond '
@@ -34,17 +51,17 @@ def call(prompt):
     'function call name exactly as I listed'
   )
 
-  func_convo = [{'role':'system','content': system},
-                {'role':'user','content': prompt}]
-  
-  try:
-    response = chat(model=MODEL_NAME, messages=func_convo)
-    return response.message.content
-  except Exception as e:
-    print(f'ERROR : {e}')
+  # func_convo = [{'role':'system','content': system},
+  #               {'role':'user','content': prompt}]
+  func_convo = f"System: {system}\nUser: {prompt}"
+
+  response = model.generate_content(func_convo)
+  return response.text
+  # response = chat(model=MODEL_NAME, messages=func_convo)
+  # return response.message.content
 
 def take_screenshot():
-  path = 'screen.jpg'
+  path = 'screenshot.jpg'
   screen = ImageGrab.grab()
   rgb_screen = screen.convert('RGB') 
   rgb_screen.save(path, quality=15)
@@ -65,11 +82,39 @@ def clipboard():
   else:
     print("No clipboard text found")
     return None
+  
+def vision_prompt(prompt, photo_path):
+  img  = Image.open(photo_path)
+  prompt = (
+    'You are the vision analysis AI that provides semtantic meaning from images to provide context '
+    'to send to another AI that will create a response to the user. Do not respond as the AI assistant '
+    'to the user. Instead take the user prompt input and try to extract all meaning from the photo '
+    'relevant to the user prompt. Then generate as much as objective data about the image for the AI '
+    f'assistant who will respond to the user. \nUSER PROMPT: {prompt}'
+  )
+  response = model.generate_content([prompt, img])
+  return response.text
 
 while True:
   prompt = input('USER : ')
-  response = gen(prompt)
-  function_call = call(prompt)
+  call = function_call(prompt)
 
-  print(f'CALL : {function_call}')
-  print(f'AI : {response}')
+  if 'take screenshot' in call:
+    print('Taking Screenshot')
+    take_screenshot()
+    visual_context = vision_prompt(prompt=prompt, photo_path='screenshot.jpg')
+  elif 'capture webcam' in call:
+    print('Capturing Webcam')
+    web_cam_capture()
+    visual_context = vision_prompt(prompt=prompt, photo_path='webcam.jpg')
+  elif 'extract clipboard' in call:
+    print('copying Clipboard')
+    paste = clipboard()
+    prompt = f'{prompt}\n\n CLIPBOARD CONTENT: {paste}'
+    visual_context = None
+  else:
+    visual_context = None
+
+  response = gen(prompt=prompt, img_context = visual_context)
+  # print(f'CONTEXT : {visual_context}')
+  print(response)
